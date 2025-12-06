23aa12022b9ed8d6d13da69614f87b5a38f4f90f
Chacha Chacha <calex2607@gmail.com>
2025-12-06 13:05:12 +0300

    Add Dotenvy for environment variable management
    - Add dotenvy dependency to mix.exs
    - Create .env.example with all configuration variables
    - Update .gitignore to exclude .env files
    - Configure dev.exs to use environment variables
    - Update README with comprehensive setup instructions
    - Document all available environment variables

--- config/dev.exs
+++ config/dev.exs
@@ -2,10 +2,10 @@ import Config
 
 # Configure your database
 config :cms, Cms.Repo,
-  username: "postgres",
-  password: "postgres",
-  hostname: "localhost",
-  database: "cms_dev",
+  username: System.get_env("DATABASE_USER") || "postgres",
+  password: System.get_env("DATABASE_PASSWORD") || "postgres",
+  hostname: System.get_env("DATABASE_HOST") || "localhost",
+  database: System.get_env("DATABASE_NAME") || "cms_dev",
   stacktrace: true,
   show_sensitive_data_on_connection_error: true,
   pool_size: 10
@@ -23,7 +23,9 @@ config :cms, CmsWeb.Endpoint,
   check_origin: false,
   code_reloader: true,
   debug_errors: true,
-  secret_key_base: "9zxw8sCSaULS34y1onOdrLZlw62hBmxGs58xI6Jl8NEp8h9PpzfJGnKJ76S62gkG",
+  secret_key_base:
+    System.get_env("SECRET_KEY_BASE") ||
+      "9zxw8sCSaULS34y1onOdrLZlw62hBmxGs58xI6Jl8NEp8h9PpzfJGnKJ76S62gkG",
   watchers: [
     esbuild: {Esbuild, :install_and_run, [:cms, ~w(--sourcemap=inline --watch)]},
     tailwind: {Tailwind, :install_and_run, [:cms, ~w(--watch)]}
